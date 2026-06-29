0f4b4f0a2c559df4940b865b6236bb292e47484b
gmukire-lang <g.mukire@alustudent.com>
2026-06-29 07:59:19 -0700

    finished doing my work on level 1 task

---
+++ quest_01_first_spell.py
@@ -0,0 +1,37 @@
+# Question 1 - Level 1
+
+print("Hello and welcome!")
+print("Get ready to start your coding adventure. Have fun and good luck!")
+
+# Question 2 - Level 1
+
+character = "Blaze"
+ability = "creating fire"
+
+print("This is", character + "!")
+print(character, "can use the power of", ability + ".")
+
+# Question 3 - Level 1
+
+my_age = 19
+best_food = "fried rice"
+
+print("I am", my_age, "years old.")
+print("My favorite food is", best_food + ".")
+
+# Question 4 - Level 1
+
+town = "Musanze"
+year = 2026
+student_name = "Chris"
+
+print(f"Hello, {student_name}! Welcome to {town}. The current year is {year}.")
+
+# Question 5 - Level 1
+
+health = 100
+
+health = health - 25
+health = health + 10
+
+print("Player's remaining health:", health)
